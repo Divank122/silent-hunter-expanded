057681c58632e702b81c065d07cd66466e09379c
Divank <Divank122@gmail.com>
2026-05-01 21:40:13 +0800

    新增护符卡牌，拆分文档结构
    - 新增护符(Amulet)：稀有技能卡，获得能量，敌方有中毒时额外效果
    - 修复护符在无可选敌人时也能打出
    - 处理卡图尺寸(镇静剂、护符)为1000x760
    - 拆分文档：README.md(基本信息)、CARDS.md(卡牌列表)、CHANGELOG.md(更新日志)

---
+++ Scripts/Cards/Amulet.cs
@@ -0,0 +1,90 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BaseLib.Abstracts;
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.Models.Powers;
+
+namespace USCE.Scripts.Cards;
+
+[Pool(typeof(SilentCardPool))]
+public class Amulet : SilentCardModel
+{
+    private const int energyCost = 1;
+    private const CardType type = CardType.Skill;
+    private const CardRarity rarity = CardRarity.Uncommon;
+    private const TargetType targetType = TargetType.AnyEnemy;
+
+    protected override bool ShouldGlowGoldInternal
+    {
+        get
+        {
+            if (CombatState == null)
+            {
+                return false;
+            }
+            return CombatState.HittableEnemies.Any(e => e.GetPower<PoisonPower>()?.Amount > 0);
+        }
+    }
+
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+    [
+        new EnergyVar(1),
+        new DynamicVar("PoisonLoss", 3m)
+    ];
+
+    protected override IEnumerable<IHoverTip> ExtraHoverTips =>
+    [
+        EnergyHoverTip
+    ];
+
+    public override List<(string, string)>? Localization => LocManager.Instance.Language switch
+    {
+        "zhs" => new CardLoc("护符", "获得{energyPrefix:energyIcons(1)}。\n如果敌方拥有[gold]中毒[/gold]，使其失去{PoisonLoss:diff()}层[gold]中毒[/gold]，额外获得{energyPrefix:energyIcons(1)}。"),
+        _ => new CardLoc("Amulet", "Gain {energyPrefix:energyIcons(1)}.\nIf the enemy has [gold]Poison[/gold], they lose {PoisonLoss:diff()} [gold]Poison[/gold] and you gain {energyPrefix:energyIcons(1)}.")
+    };
+
+    public Amulet() : base(energyCost, type, rarity, targetType)
+    {
+    }
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        int energyGain = DynamicVars.Energy.IntValue;
+        int poisonLoss = DynamicVars["PoisonLoss"].IntValue;
+
+        await PlayerCmd.GainEnergy(energyGain, Owner);
+
+        if (cardPlay.Target != null)
+        {
+            var poisonPower = cardPlay.Target.GetPower<PoisonPower>();
+            if (poisonPower != null && poisonPower.Amount > 0)
+            {
+                int newAmount = poisonPower.Amount - poisonLoss;
+                if (newAmount <= 0)
+                {
+                    await PowerCmd.Remove(poisonPower);
+                }
+                else
+                {
+                    poisonPower.SetAmount(newAmount);
+                }
+
+                await PlayerCmd.GainEnergy(energyGain, Owner);
+            }
+        }
+    }
+
+    protected override void OnUpgrade()
+    {
+        base.EnergyCost.UpgradeBy(-1);
+    }
+}
