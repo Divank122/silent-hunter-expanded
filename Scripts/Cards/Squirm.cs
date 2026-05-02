d0dbddc21c64d88d03e0c88a39021272ba9845fd
Divank <Divank122@gmail.com>
2026-05-02 16:12:14 +0800

    新增蠕动卡牌，修复针锋相对和剥皮 (v0.0.11)

---
+++ Scripts/Cards/Squirm.cs
@@ -0,0 +1,95 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BaseLib.Abstracts;
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.Models.Powers;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace USCE.Scripts.Cards;
+
+[Pool(typeof(SilentCardPool))]
+public class Squirm : SilentCardModel
+{
+    private const int energyCost = 1;
+    private const CardType type = CardType.Skill;
+    private const CardRarity rarity = CardRarity.Uncommon;
+    private const TargetType targetType = TargetType.AnyEnemy;
+
+    public override bool GainsBlock => true;
+
+    protected override bool ShouldGlowGoldInternal
+    {
+        get
+        {
+            if (CombatState == null)
+            {
+                return false;
+            }
+            return CombatState.HittableEnemies.Any(e => e.GetPower<PoisonPower>()?.Amount > 0);
+        }
+    }
+
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+    [
+        new BlockVar(8m, ValueProp.Move),
+        new DynamicVar("PoisonLoss", 3m),
+        new BlockVar("BonusBlock", 6m, ValueProp.Move)
+    ];
+
+    protected override IEnumerable<IHoverTip> ExtraHoverTips =>
+    [
+        HoverTipFactory.FromPower<PoisonPower>()
+    ];
+
+    public override List<(string, string)>? Localization => LocManager.Instance.Language switch
+    {
+        "zhs" => new CardLoc("蠕动", "获得{Block:diff()}点格挡。\n如果敌方拥有[gold]中毒[/gold]，使其失去{PoisonLoss:diff()}层[gold]中毒[/gold]，额外获得{BonusBlock:diff()}点格挡。"),
+        _ => new CardLoc("Squirm", "Gain {Block:diff()} Block.\nIf the enemy has [gold]Poison[/gold], they lose {PoisonLoss:diff()} [gold]Poison[/gold] and you gain {BonusBlock:diff()} Block.")
+    };
+
+    public Squirm() : base(energyCost, type, rarity, targetType)
+    {
+    }
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        await CreatureCmd.GainBlock(Owner.Creature, DynamicVars.Block, cardPlay);
+
+        if (cardPlay.Target != null)
+        {
+            var poisonPower = cardPlay.Target.GetPower<PoisonPower>();
+            if (poisonPower != null && poisonPower.Amount > 0)
+            {
+                int poisonLoss = DynamicVars["PoisonLoss"].IntValue;
+
+                int newAmount = poisonPower.Amount - poisonLoss;
+                if (newAmount <= 0)
+                {
+                    await PowerCmd.Remove(poisonPower);
+                }
+                else
+                {
+                    poisonPower.SetAmount(newAmount);
+                }
+
+                var bonusBlockVar = (BlockVar)DynamicVars["BonusBlock"];
+                await CreatureCmd.GainBlock(Owner.Creature, bonusBlockVar, cardPlay);
+            }
+        }
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Block.UpgradeValueBy(3m);
+        DynamicVars["BonusBlock"].UpgradeValueBy(2m);
+    }
+}
---
+++ Scripts/Patches/SquirmTargetPatch.cs
@@ -0,0 +1,74 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Godot;
+using HarmonyLib;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.Nodes.Combat;
+using USCE.Scripts.Cards;
+
+namespace USCE.Scripts.Patches;
+
+[HarmonyPatch(typeof(NMouseCardPlay))]
+public static class SquirmMouseTargetPatch
+{
+    [HarmonyPatch("TargetSelection")]
+    [HarmonyPrefix]
+    public static bool TargetSelectionPrefix(NMouseCardPlay __instance, ref Task __result, TargetMode targetMode)
+    {
+        var card = __instance.Holder?.CardNode?.Model;
+        if (card == null) return true;
+
+        if (card.GetType() != typeof(Squirm)) return true;
+
+        var combatState = card.CombatState;
+        if (combatState == null) return true;
+
+        var hittableEnemies = combatState.HittableEnemies;
+        if (hittableEnemies.Count > 0) return true;
+
+        __result = Task.CompletedTask;
+        return false;
+    }
+}
+
+[HarmonyPatch(typeof(NControllerCardPlay))]
+public static class SquirmControllerTargetPatch
+{
+    private static readonly MethodInfo? TryPlayCardMethod = typeof(NCardPlay).GetMethod("TryPlayCard", BindingFlags.Instance | BindingFlags.NonPublic);
+
+    [HarmonyPatch("SingleCreatureTargeting")]
+    [HarmonyPrefix]
+    public static bool SingleCreatureTargetingPrefix(NControllerCardPlay __instance, ref Task __result, TargetType targetType)
+    {
+        var card = __instance.Holder?.CardNode?.Model;
+        if (card == null) return true;
+
+        if (card.GetType() != typeof(Squirm)) return true;
+
+        var combatState = card.CombatState;
+        if (combatState == null) return true;
+
+        var owner = card.Owner?.Creature;
+        if (owner == null) return true;
+
+        List<Creature> list = new List<Creature>();
+        switch (targetType)
+        {
+            case TargetType.AnyEnemy:
+                list = owner.CombatState?.GetOpponentsOf(owner)?.Where(c => c.IsHittable)?.ToList() ?? new List<Creature>();
+                break;
+            case TargetType.AnyAlly:
+                list = card.CombatState?.PlayerCreatures?.Where(c => c.IsHittable && c != owner)?.ToList() ?? new List<Creature>();
+                break;
+        }
+        if (list.Count > 0) return true;
+
+        TryPlayCardMethod?.Invoke(__instance, new object?[] { null });
+        __result = Task.CompletedTask;
+        return false;
+    }
+}
