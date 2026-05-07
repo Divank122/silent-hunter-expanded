578b23c4486d18a907e6f1bd0c4f098731aeaf30
Divank <Divank122@gmail.com>
2026-05-08 00:06:51 +0800

    新增卡牌：消解（稀有技能，弃牌返回手牌）

---
+++ Scripts/Cards/Dissolve.cs
@@ -0,0 +1,67 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using BaseLib.Abstracts;
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.Models.Powers;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace USCE.Scripts.Cards;
+
+[Pool(typeof(SilentCardPool))]
+public class Dissolve : SilentCardModel
+{
+    private const int energyCost = 0;
+    private const CardType type = CardType.Skill;
+    private const CardRarity rarity = CardRarity.Rare;
+    private const TargetType targetType = TargetType.AnyEnemy;
+
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+    [
+        new DynamicVar("PoisonAmount", 2m)
+    ];
+
+    protected override IEnumerable<IHoverTip> ExtraHoverTips =>
+    [
+        HoverTipFactory.FromPower<PoisonPower>()
+    ];
+
+    public override List<(string, string)>? Localization => LocManager.Instance.Language switch
+    {
+        "zhs" => new CardLoc("消解", "给予{PoisonAmount:diff()}层[gold]中毒[/gold]。\n每当你弃牌时，将这张牌放入你的[gold]手牌[/gold]。"),
+        _ => new CardLoc("Dissolve", "Apply {PoisonAmount:diff()} [gold]Poison[/gold].\nWhenever you discard, put this card into your [gold]hand[/gold].")
+    };
+
+    public Dissolve() : base(energyCost, type, rarity, targetType)
+    {
+    }
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        if (cardPlay.Target != null)
+        {
+            int poisonAmount = DynamicVars["PoisonAmount"].IntValue;
+            await PowerCmd.Apply<PoisonPower>(cardPlay.Target, poisonAmount, Owner.Creature, this);
+        }
+    }
+
+    public override async Task AfterCardDiscarded(PlayerChoiceContext choiceContext, CardModel card)
+    {
+        if (card == this && Pile != null && Pile.Type == PileType.Discard)
+        {
+            await CardPileCmd.Add(this, PileType.Hand);
+        }
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars["PoisonAmount"].UpgradeValueBy(1m);
+    }
+}
