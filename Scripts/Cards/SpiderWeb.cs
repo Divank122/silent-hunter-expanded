cba1af035b68be7690b26187451bfa1c85bf6504
Divank <Divank122@gmail.com>
2026-05-12 02:58:37 +0800

    rework: 蛛网重做为攻击时给予虚弱和易伤，添加hovertip

--- Scripts/Cards/SpiderWeb.cs
+++ Scripts/Cards/SpiderWeb.cs
@@ -5,6 +5,7 @@ using BaseLib.Utils;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.Models.CardPools;
@@ -19,20 +20,25 @@ public class SpiderWeb : SilentCardModel, ILocalizationProvider
     private const int energyCost = 1;
     private const CardType type = CardType.Skill;
     private const CardRarity rarity = CardRarity.Uncommon;
-    private const TargetType targetType = TargetType.AnyEnemy;
+    private const TargetType targetType = TargetType.Self;
 
     protected override IEnumerable<DynamicVar> CanonicalVars =>
     [
-        new DynamicVar("InitialWeak", 1m),
-        new DynamicVar("FollowUpWeak", 1m)
+        new PowerVar<SpiderWebPower>(1m)
     ];
 
     public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Exhaust];
 
+    protected override IEnumerable<IHoverTip> ExtraHoverTips =>
+    [
+        HoverTipFactory.FromPower<WeakPower>(),
+        HoverTipFactory.FromPower<VulnerablePower>()
+    ];
+
     public override List<(string, string)>? Localization => LocManager.Instance.Language switch
     {
-        "zhs" => new CardLoc("蛛网", "给予{InitialWeak:diff()}层[gold]虚弱[/gold]。\n你在这个回合内该名敌人每次受到伤害时，额外给予{FollowUpWeak:diff()}层[gold]虚弱[/gold]。"),
-        _ => new CardLoc("Spider Web", "Apply {InitialWeak:diff()} [gold]Weak[/gold].\nEach time that enemy takes damage this turn, apply {FollowUpWeak:diff()} additional [gold]Weak[/gold].")
+        "zhs" => new CardLoc("蛛网", "在这个回合，每当你攻击敌人时，给予{SpiderWebPower:diff()}层[gold]虚弱[/gold]和{SpiderWebPower:diff()}层[gold]易伤[/gold]。"),
+        _ => new CardLoc("Spider Web", "This turn, whenever you attack an enemy, apply {SpiderWebPower:diff()} [gold]Weak[/gold] and {SpiderWebPower:diff()} [gold]Vulnerable[/gold].")
     };
 
     public SpiderWeb() : base(energyCost, type, rarity, targetType)
@@ -41,22 +47,12 @@ public class SpiderWeb : SilentCardModel, ILocalizationProvider
 
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
     {
-        if (cardPlay.Target == null) return;
-
-        int initialWeak = DynamicVars["InitialWeak"].IntValue;
-        int followUpWeak = DynamicVars["FollowUpWeak"].IntValue;
-        
-        await PowerCmd.Apply<WeakPower>(cardPlay.Target, initialWeak, Owner.Creature, this);
-        
-        var power = await PowerCmd.Apply<SpiderWebPower>(cardPlay.Target, followUpWeak, Owner.Creature, this);
-        if (power != null)
-        {
-            power.SourcePlayer = Owner.Creature;
-        }
+        await CreatureCmd.TriggerAnim(Owner.Creature, "Cast", Owner.Character.CastAnimDelay);
+        await PowerCmd.Apply<SpiderWebPower>(Owner.Creature, DynamicVars["SpiderWebPower"].IntValue, Owner.Creature, this);
     }
 
     protected override void OnUpgrade()
     {
-        DynamicVars["InitialWeak"].UpgradeValueBy(1m);
+        DynamicVars.Energy.UpgradeValueBy(-1m);
     }
 }
--- Scripts/Powers/SpiderWebPower.cs
+++ Scripts/Powers/SpiderWebPower.cs
@@ -1,11 +1,13 @@
+using System.Linq;
 using System.Threading.Tasks;
 using BaseLib.Abstracts;
-using BaseLib.Extensions;
 using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Commands.Builders;
 using MegaCrit.Sts2.Core.Combat;
 using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.Entities.Powers;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization;
 using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.Models.Powers;
@@ -15,27 +17,46 @@ namespace USCE.Scripts.Powers;
 
 public class SpiderWebPower : CustomPowerModel
 {
-    public override PowerType Type => PowerType.Debuff;
+    public override PowerType Type => PowerType.Buff;
     public override PowerStackType StackType => PowerStackType.Counter;
 
     public override string? CustomPackedIconPath => "res://UltimateSilentCardExpansion/images/powers/usce_spider_web_power.png";
     public override string? CustomBigIconPath => "res://UltimateSilentCardExpansion/images/powers/usce_spider_web_power.png";
 
-    public Creature? SourcePlayer { get; set; }
+    protected override IEnumerable<IHoverTip> ExtraHoverTips =>
+    [
+        HoverTipFactory.FromPower<WeakPower>(),
+        HoverTipFactory.FromPower<VulnerablePower>()
+    ];
 
     public override List<(string, string)>? Localization => LocManager.Instance.Language switch
     {
-        "zhs" => new PowerLoc("蛛网", "本回合内每次受到伤害时，获得1层虚弱。", "本回合内每次受到伤害时，获得[blue]{Amount}[/blue]层[gold]虚弱[/gold]。"),
-        _ => new PowerLoc("Spider Web", "Each time takes damage this turn, gain 1 Weak.", "Each time takes damage this turn, gain [blue]{Amount}[/blue] [gold]Weak[/gold].")
+        "zhs" => new PowerLoc("蛛网", "本回合每当你攻击敌人时，给予1层虚弱和1层易伤。", "本回合每当你攻击敌人时，给予[blue]{Amount}[/blue]层[gold]虚弱[/gold]和[blue]{Amount}[/blue]层[gold]易伤[/gold]。"),
+        _ => new PowerLoc("Spider Web", "This turn, whenever you attack an enemy, apply 1 Weak and 1 Vulnerable.", "This turn, whenever you attack an enemy, apply [blue]{Amount}[/blue] [gold]Weak[/gold] and [blue]{Amount}[/blue] [gold]Vulnerable[/gold].")
     };
 
-    public override async Task AfterDamageReceived(PlayerChoiceContext choiceContext, Creature target, DamageResult result, ValueProp props, Creature? dealer, CardModel? cardSource)
+    public override async Task AfterAttack(AttackCommand command)
     {
-        if (target == Owner && result.TotalDamage > 0)
+        if (command.Attacker != Owner || command.TargetSide == Owner.Side)
         {
-            await PowerCmd.Apply<WeakPower>(Owner, Amount, SourcePlayer, null);
-            Flash();
+            return;
         }
+
+        if (!command.DamageProps.IsPoweredAttack())
+        {
+            return;
+        }
+
+        foreach (DamageResult result in command.Results)
+        {
+            if (result.TotalDamage > 0 && !result.Receiver.IsDead)
+            {
+                await PowerCmd.Apply<WeakPower>(result.Receiver, Amount, Owner, null);
+                await PowerCmd.Apply<VulnerablePower>(result.Receiver, Amount, Owner, null);
+            }
+        }
+
+        Flash();
     }
 
     public override async Task AfterTurnEnd(PlayerChoiceContext choiceContext, CombatSide side)
