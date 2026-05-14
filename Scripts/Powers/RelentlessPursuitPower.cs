b92ee2100f61743d5208b11998b594bc3628675a
Divank <Divank122@gmail.com>
2026-05-14 23:11:17 +0800

    fix: 修复穷追不舍升级后描述不更新和图标未正确显示层数的问题

--- Scripts/Powers/RelentlessPursuitPower.cs
+++ Scripts/Powers/RelentlessPursuitPower.cs
@@ -15,7 +15,7 @@ namespace USCE.Scripts.Powers;
 public class RelentlessPursuitPower : CustomPowerModel
 {
     public override PowerType Type => PowerType.Buff;
-    public override PowerStackType StackType => PowerStackType.Single;
+    public override PowerStackType StackType => PowerStackType.Counter;
 
     public override string? CustomPackedIconPath => "res://UltimateSilentCardExpansion/images/powers/usce_relentless_pursuit_power.png";
     public override string? CustomBigIconPath => "res://UltimateSilentCardExpansion/images/powers/usce_relentless_pursuit_power.png";
@@ -24,44 +24,6 @@ public class RelentlessPursuitPower : CustomPowerModel
     {
         "zhs" => new PowerLoc("穷追不舍", "打出攻击牌时，如果你的手牌中有与其名字相同的牌，额外造成4点伤害。", "打出攻击牌时，如果你的[gold]手牌[/gold]中有与其名字相同的牌，额外造成[blue]{Amount}[/blue]点伤害。"),
         _ => new PowerLoc("Relentless Pursuit", "When you play an Attack, if you have a card with the same name in your hand, deal 4 extra damage.", "When you play an Attack, if you have a card with the same name in your [gold]hand[/gold], deal [blue]{Amount}[/blue] extra damage.")
-    };
-
-    public override decimal ModifyDamageAdditive(Creature? target, decimal amount, ValueProp props, Creature? dealer, CardModel? cardSource)
-    {
-        if (cardSource == null || cardSource.Type != CardType.Attack)
-        {
-            return 0m;
-        }
-        if (dealer != Owner)
-        {
-            return 0m;
-        }
-        var hand = Owner.Player?.PlayerCombatState?.Hand?.Cards;
-        if (hand == null)
-        {
-            return 0m;
-        }
-        bool hasSameName = hand.Any(c => c != cardSource && c.Id == cardSource.Id);
-        if (!hasSameName)
-        {
-            return 0m;
-        }
-        return Amount;
-    }
-}
-
-public class RelentlessPursuitPowerPlus : CustomPowerModel
-{
-    public override PowerType Type => PowerType.Buff;
-    public override PowerStackType StackType => PowerStackType.Single;
-
-    public override string? CustomPackedIconPath => "res://UltimateSilentCardExpansion/images/powers/usce_relentless_pursuit_power.png";
-    public override string? CustomBigIconPath => "res://UltimateSilentCardExpansion/images/powers/usce_relentless_pursuit_power.png";
-
-    public override List<(string, string)>? Localization => LocManager.Instance.Language switch
-    {
-        "zhs" => new PowerLoc("穷追不舍+", "打出攻击牌时，如果你的手牌中有与其名字相同的牌，额外造成6点伤害。", "打出攻击牌时，如果你的[gold]手牌[/gold]中有与其名字相同的牌，额外造成[blue]{Amount}[/blue]点伤害。"),
-        _ => new PowerLoc("Relentless Pursuit+", "When you play an Attack, if you have a card with the same name in your hand, deal 6 extra damage.", "When you play an Attack, if you have a card with the same name in your [gold]hand[/gold], deal [blue]{Amount}[/blue] extra damage.")
     };
 
     public override decimal ModifyDamageAdditive(Creature? target, decimal amount, ValueProp props, Creature? dealer, CardModel? cardSource)
